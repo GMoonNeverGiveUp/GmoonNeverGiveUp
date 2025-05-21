8402ba5eef4492d5a446136150f8953693a92a3f
GMoonNeverGiveUp <web3negentropy@gmail.com>
2025-05-21 18:56:36 +0000

    Site name typo error fix: astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import staticAdapter from '@astrojs/adapter-static';
 export default defineConfig({
   output: 'static',
   adapter: staticAdapter(),
-  site: 'https://gmoonnervergiveup.on.fleek.co/'
+  site: 'https://gmoonnevergiveup.on.fleek.co/'
 });

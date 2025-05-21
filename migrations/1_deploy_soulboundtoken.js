37deab7b1451bd3c5134a7bda3afcaa878984b88
GMoonNeverGiveUp <web3negentropy@gmail.com>
2025-05-21 12:12:32 -0300

    Delete migrations directory

--- migrations/1_deploy_soulboundtoken.js
+++
@@ -1,6 +0,0 @@
-const SoulBoundToken = artifacts.require("SoulBoundToken");
-
-module.exports = async function (deployer) {
-  // Deploy with an increased gas limit of 15,000,000
-  await deployer.deploy(SoulBoundToken, { gas: 15000000 });
-};
--- migrations/2_deploy_memecontest.js
+++
@@ -1,7 +0,0 @@
-const MemeContest = artifacts.require("MemeContest");
-const SoulBoundToken = artifacts.require("SoulBoundToken");
-
-module.exports = async function (deployer) {
-  const sbtInstance = await SoulBoundToken.deployed();
-  await deployer.deploy(MemeContest, sbtInstance.address);
-};

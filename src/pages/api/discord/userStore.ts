2da81aee2b0b858a9c49f03d1e56ec9049128ec4
GMoonNeverGiveUp <web3negentropy@gmail.com>
2025-05-21 12:36:33 -0300

    Delete src/pages/api directory

--- src/pages/api/discord/userStore.ts
+++
@@ -1,9 +0,0 @@
-import { atom } from 'nanostores';
-
-export interface User {
-  discordId: string;
-  username: string;
-  role: 'Spectator' | 'Galactic G' | 'Guardian';
-}
-
-export const userStore = atom<User | null>(null);
